0db625ad69025c6ca60373ef2d4a38939184ad81
TWoerner94 <timwoerner94@web.de>
2018-01-24 11:34:56 +0100

    formatting fixes

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   
   var api = "https://en.wikipedia.org/w/api.php";
+  var outputField = $('#output');
   
   $(".submitBtn").on("click", function() {
     var searchInput = $('#searchTerm').val();
     var resultsUrl = api+"?action=query&list=search&srsearch="+searchInput+"&format=json&callback=?";
     $.getJSON(resultsUrl, function(data){
-      $('#output').empty();
-      $('#output').append("Results for <strong>" + searchInput + "</strong>");
+      outputField.empty();
+      outputField.append("Results for <strong>" + searchInput + "</strong>");
       $.each(data.query.search, function(i, item) {
-        $('#output').append("<li><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + " </a>" + item.snippet + "</li>");
+        outputField.append("<li><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + " </a>" + item.snippet + "</li>");
       });
     });
   });
